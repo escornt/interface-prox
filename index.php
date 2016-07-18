90f3e905d90d3eadcd010c8cf05d86ecafaa1322
Thomas Escorne <thomas.escorne@1001pneus.local>
2016-07-18 15:53:43 +0200

    test

--- index.php
+++ index.php
@@ -1,12 +1,10 @@
-<?php
-session_start(); $username = $password = $userError = $passError = '';
-echo "pls";
-if(isset($_POST['sub'])){
-  $username = $_POST['username']; $password = $_POST['password'];
-  if($username === 'admin' && $password === 'password'){
-    $_SESSION['login'] = true; header('LOCATION:wherever.php'); die();
-  }
-  if($username !== 'admin')$userError = 'Invalid Username';
-  if($password !== 'password')$passError = 'Invalid Password';
-}
-?>
+<!DOCTYPE html>
+<html>
+  <head>
+      <meta charset="utf-8" />
+      <title>Interface proxmox</title>
+  </head>
+  <body>
+      <h1>Phase de test | Portail de login</h1>
+  </body>
+</html>
