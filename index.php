3c805c65804096d673584a354e67236e39868a29
Thomas Escorne <thomas.escorne@1001pneus.local>
2016-07-18 16:02:53 +0200

    test

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
   <head>
       <meta charset="utf-8" />
       <title>Interface proxmox</title>
+      <link rel="stylesheet" href="test-style.css" />
   </head>
   <body>
       <h1>Phase de test | Portail de login</h1>
