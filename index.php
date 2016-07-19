6cf9becdcc340baf3f52b62509116f0e99090bf4
escornt <thomas.escorne@epitech.eu>
2016-07-19 12:02:09 +0200

    test

--- index.php
+++ index.php
@@ -5,26 +5,29 @@
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <title>Interface proxmox</title>
       <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+      <link href="test-style.css" />
       <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
   <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
   </head>
-  <body>
+  <body backgound="montain.jpeg">
     <div class="container">
       <h1>Interface de création de conteneurs Proxmox<small> Phase de test</small></h1>
     </div>
+&nbsp;
     <div class="container">
-      <form class="form-inline" role="form">
+      <form class="form-vertical" role="form">
         <div class="form-group">
-          <label class="sr-only" for="email">Nom d'utilisateur :</label>
-          <input type="email" class="form-control" id="email">
+          <label for="usr">Nom d'utilisateur :</label>
+          <input type="usr" class="form-control" id="usr">
         </div>
         <div class="form-group">
-          <label class="sr-only" for="pwd">Mot de passe :</label>
+          <label for="pwd">Mot de passe :</label>
           <input type="password" class="form-control" id="pwd">
         </div>
         <div class="checkbox">
           <label><input type="checkbox"> Remember me</label>
         </div>
+	<button type="submit" class="btn btn-info">Submit</button>
       </form>
     </div>
   </body>
