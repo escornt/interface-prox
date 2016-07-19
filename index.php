a987dcba343de09f72d85c584ef6e7419fc3e1d7
escornt <thomas.escorne@epitech.eu>
2016-07-19 11:24:56 +0200

    ebauche bootstrap

--- index.php
+++ index.php
@@ -1,12 +1,23 @@
 <!DOCTYPE html>
-<html>
+<html lang="fr">
   <head>
       <meta charset="utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
       <title>Interface proxmox</title>
-      <link rel="stylesheet" href="test-style.css" />
+      <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
   </head>
   <body>
-      <h1>Phase de test | Portail de login</h1>
-      <p>Cette phrase devrais etre rouge</p>
+    <div class="container">
+      <h1>Phase de test <small>Portail de login</small></h1>
+    </div>
+    <div class="container-fluid">
+      <p><abbr title="test">petit bout de texte</abbr><blockquote>Ceci est mis en avant par blockquote <footer>Thomas</footer></blockquote></p>
+      <p class="pre-scrollable"><pre>Coucou</pre></p>
+      <table class="table">
+	
+      </table>
+    </div>
   </body>
 </html>
