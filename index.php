338d702200e19aa194aa1a3c6b201230c3b8a1e6
Thomas Escorne <thomas.escorne@1001pneus.local>
2016-07-19 16:50:04 +0200

    +

--- index.php
+++ index.php
@@ -6,13 +6,14 @@
       <title>Interface proxmox</title>
       <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
       <link rel="stylesheet" href="style.css" />
+      <link href="https://fonts.googleapis.com/css?family=Changa" rel="stylesheet">
       <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
   <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
   </head>
   <body>
     <div class="row">
         <div class="col-xs-4 col-md-offset-4">
-          <h1 class="m1"><b>Interface de création de conteneurs Proxmox</b></h1>
+          <h1 class="m1 customfont"><b>Interface de création de conteneurs Proxmox</b></h1>
         </div>
     </div>
   <div class="row">
