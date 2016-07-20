19824c51739c94b2e75b66846d97c6a193e19eec
Thomas Escorne <thomas.escorne@1001pneus.local>
2016-07-20 15:26:41 +0200

    test

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once("config/global/config.php");
-require_once("config/include.php");
+/*require_once("config/global/config.php");
+require_once("config/include.php");*/
 
 include_once('view/login.php');
