35806161a729ded430b6bcc9072a2d9508e77f83
Thomas Escorne <thomas.escorne@1001pneus.local>
2016-07-21 09:52:49 +0200

    test pour redirection

--- controller/sub_handler.php
+++ controller/sub_handler.php
@@ -11,5 +11,5 @@ if (strcmp($_POST["user"], "OK") != 0  || strcmp($_POST["pswd"], "OK") != 0)
 else {
   $_SESSION['logstate'] = 0;
 }
-include $_SERVER['DOCUMENT_ROOT'].'/view/login.php';
+include $_SERVER['DOCUMENT_ROOT']."/view/login.php";
 ?>
