b55984d80f5f52632d9363914a3de04ed34f1a55
Thomas Escorne <thomas.escorne@1001pneus.local>
2016-07-21 15:03:45 +0200

    test

--- controller/sub_handler.php
+++ controller/sub_handler.php
@@ -11,14 +11,29 @@ session_start();
 else {
   $_SESSION['logstate'] = 0;
 }*/
-$ds=ldap_connect("10.100.1.18:389");
+$ds=ldap_connect("10.100.1.18:389")
 $r=ldap_bind($ds, $_POST['user'] ."@1001PNEUS.LOCAL", $_POST['pswd']);
 if ($r == false || empty($_POST['user']) || empty($_POST['pswd'])){
   $_SESSION['logstate'] = 1;
   header('Location: http://interface-prox.www.1001pneus.fr/view/login.php');
 }
 else {
-  $_SESSION['logstate'] = 1;
-  echo 'coucou';
+  $_SESSION['logstate'] = 0;
+  print_r(InitializeDroitsFromDom($ds));
 }
+
+function InitializeDroitsFromDom($ds) {
+  $droits = array();
+  $dn = "DC=1001pneus,DC=local";
+  $filter = "(&(&(&(objectClass=group))))";
+  $search = @ldap_search($ds, $dn, $filter) or die("ldap search failed");
+  $entries = ldap_get_entries($ds, $search);
+  foreach ($entries as $e) {
+    if (preg_match("/intranet_(.*)/", $e[cn][0], $m)) {
+      $droits[$m[1]] = 0;
+    }
+  }
+  return($droits);
+}
+
 ?>
