dae7fddda48570a12e8d4d5b7edde2d62363aa1b
Thomas Escorne <thomas.escorne@1001pneus.local>
2016-07-21 11:31:10 +0200

    test

--- controller/sub_handler.php
+++ controller/sub_handler.php
@@ -4,13 +4,18 @@ session_start();
 /*Start connection LDAP*/
 /*Do Authentication*/
 /*Test Result*/
-if (strcmp($_POST["user"], "OK") != 0  || strcmp($_POST["pswd"], "OK") != 0)
+/*if (strcmp($_POST["user"], "OK") != 0  || strcmp($_POST["pswd"], "OK") != 0)
 {
   $_SESSION['logstate'] = 1;
 }
 else {
   $_SESSION['logstate'] = 0;
-}
+}*/
+$ds=ldap_connect(10.100.1.18:389);
+if (!$ds){
 header('Location: http://interface-prox.www.1001pneus.fr/view/login.php');
-
+}
+else {
+  echo 'coucou';
+}
 ?>
