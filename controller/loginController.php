4b7d8944eaa2e28c243d7e740540422a726b0fdd
Thomas Escorne <thomas.escorne@1001pneus.local>
2016-07-22 12:17:50 +0200

    gestion des droits

--- controller/loginController.php
+++ controller/loginController.php
@@ -46,7 +46,7 @@ function ConnectToDomain($_Login, $Pass, &$Infos, &$InfosDroits, $server) {
   ldap_set_option($ds, LDAP_OPT_REFERRALS, 0);
   ldap_set_option($ds, LDAP_OPT_PROTOCOL_VERSION, 3);
   $r = @ldap_bind($ds, "$Login@1001PNEUS.LOCAL", "$Pass");
-  if ($r && || !empty($_POST['user']) || !empty($_POST['pswd'])) {
+  if ($r && !empty($_POST['user']) && !empty($_POST['pswd'])) {
     $Infos = LoadInfosFromDomaine($ds, $Login);
     $Infos[TypeConnexion] = "Domaine";
     $InfoDroits = InitializeDroitsFromDom($ds);
