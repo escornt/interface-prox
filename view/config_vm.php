36d803c7418905ba6568f013f45bc6ebb49f5b92
Thomas Escorne <thomas.escorne@1001pneus.local>
2016-07-22 16:08:33 +0200

    test

--- view/config_vm.php
+++ view/config_vm.php
@@ -18,18 +18,18 @@
         </div>
     </div><!-- /.row -->
     <div class="row">
-      <div class="col-xs-6 col-md-offset-2">
+      <div class="col-xs-4 col-md-offset-4">
           <form class="form-vertical" role="form" method="POST" action="/controller/loginController.php">
 
             <div class="form-group has-feedback">
               <label for="ID VM" class="blu customfont">ID VM</label>
-              <input type="id" name="ID" class="form-control" id="ID VM" placeholder="(ex : 145)">
+              <input type="number" name="ID" class="form-control" id="ID VM" placeholder="(ex : 145)">
               <span class="glyphicon glyphicon-th-large form-control-feedback blu"></span>
             </div>
 
             <div class="form-group has-feedback">
               <label for="nom_vm" class="blu customfont">Nom VM</label>
-              <input type="nom_vm" name="nom_vm" class="form-control" id="nom_vm" placeholder="(ex : VM145)">
+              <input type="text" name="nom_vm" class="form-control" id="nom_vm" placeholder="(ex : VM145)">
               <span class="glyphicon glyphicon-tag form-control-feedback blu"></span>
             </div>
 
@@ -37,6 +37,18 @@
               <label for="password" class="blu customfont">Mot de passe</label>
               <input type="password" name="password" class="form-control" id="password" placeholder="(ex : password)">
               <span class="glyphicon glyphicon-lock form-control-feedback blu"></span>
+            </div>
+
+            <div class="form-group has-feedback">
+              <label for="password" class="blu customfont">Confirmer mot de passe</label>
+              <input type="password" name="conf-password" class="form-control" id="conf-password" placeholder="(ex : password)">
+              <span class="glyphicon glyphicon-lock form-control-feedback blu"></span>
+            </div>
+
+            <div class="form-group has-feedback">
+              <label for="Espace disque" class="blu customfont">Espace Disque (MB)</label>
+              <input type="number" name="disk_size" class="form-control" id="disk_size" placeholder="(recommandé : 4000)">
+              <span class="glyphicon glyphicon-hdd form-control-feedback blu"></span>
             </div>
 
   	        <button type="submit" class="btn btn-info customfont">Créer</button>
