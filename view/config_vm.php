e1a02527417349c43ecb64aa90e95c83819b2fb1
Thomas Escorne <thomas.escorne@1001pneus.local>
2016-07-22 17:09:36 +0200

    test

--- view/config_vm.php
+++ view/config_vm.php
@@ -78,8 +78,7 @@
             </div>
 
             <div class="form-group has-feedback">
-              <label class="customfont blu"><input type="checkbox" id="cbox2" name="update"> Mettre les sources à jour</label>
-              <small><em>Nécessite le montage du filer</em></small>
+              <label class="customfont blu"><input type="checkbox" id="cbox2" name="update"> Mettre les sources à jour <small><em>Nécessite le montage du filer</em></small></label>
             </div>
 
   	        <button type="submit" class="btn btn-info customfont">Créer</button>
