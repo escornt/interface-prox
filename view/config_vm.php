0b629c1330248bb4196fdb5694270cbb117418f3
Thomas Escorne <thomas.escorne@1001pneus.local>
2016-07-22 15:32:33 +0200

    test

--- view/config_vm.php
+++ view/config_vm.php
@@ -3,7 +3,7 @@
   <head>
       <meta charset="utf-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <title>Interface Proxmox</title>
+      <title>Interface Proxmox test</title>
       <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
       <link rel="stylesheet" href="css/style.css" />
       <link rel="shortcut icon" href="/css/favicon.ico" />
