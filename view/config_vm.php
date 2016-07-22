91e94e1f6b9b7b72b3718672d82163067422d9eb
Thomas Escorne <thomas.escorne@1001pneus.local>
2016-07-22 16:27:53 +0200

    test

--- view/config_vm.php
+++ view/config_vm.php
@@ -20,36 +20,43 @@
     <div class="row">
       <div class="col-xs-4 col-md-offset-4">
           <form class="form-vertical" role="form" method="POST" action="/controller/loginController.php">
-
+<!-- ID VM -->
             <div class="form-group has-feedback">
               <label for="ID VM" class="blu customfont">ID VM</label>
               <input type="number" name="ID" class="form-control" id="ID VM" placeholder="(ex : 145)">
               <span class="glyphicon glyphicon-th-large form-control-feedback blu"></span>
             </div>
-
+<!-- Nom VM -->
             <div class="form-group has-feedback">
               <label for="nom_vm" class="blu customfont">Nom VM</label>
               <input type="text" name="nom_vm" class="form-control" id="nom_vm" placeholder="(ex : VM145)">
               <span class="glyphicon glyphicon-tag form-control-feedback blu"></span>
             </div>
-
+<!-- Mot de passe -->
             <div class="form-group has-feedback">
               <label for="password" class="blu customfont">Mot de passe</label>
               <input type="password" name="password" class="form-control" id="password" placeholder="(ex : password)">
               <span class="glyphicon glyphicon-lock form-control-feedback blu"></span>
             </div>
-
+<!-- Confimation mot de passe -->
             <div class="form-group has-feedback">
               <label for="password" class="blu customfont">Confirmer mot de passe</label>
               <input type="password" name="conf-password" class="form-control" id="conf-password" placeholder="(ex : password)">
               <span class="glyphicon glyphicon-lock form-control-feedback blu"></span>
             </div>
-
+<!-- Espace Disque -->
             <div class="form-group has-feedback">
-              <label for="Espace disque" class="blu customfont">Espace Disque (MB)</label>
-              <input type="number" min="512" name="disk_size" class="form-control" id="disk_size" value="512">
+              <label for="Espace disque" class="blu customfont">Espace Disque (BG)</label>
+              <input type="number" min="1" name="disk_size" class="form-control" id="disk_size" value="1">
               <span class="glyphicon glyphicon-hdd form-control-feedback blu"></span>
-              <div class="m3"><em><small>Recommandation => 4000 MB</small></em></div>
+              <div class="m3"><em><small>Recommandation mini --> 15 GB</small></em></div>
+            </div>
+<!-- RAM -->
+            <div class="form-group has-feedback">
+              <label for="RAM" class="blu customfont">RAM (MB)</label>
+              <input type="number" min="512" name="RAM" class="form-control" id="RAM" value="512">
+              <span class="glyphicon glyphicon-hdd form-control-feedback blu"></span>
+              <div class="m3"><em><small>Recommandation mini --> 4000 MB </small></em></div>
             </div>
 
   	        <button type="submit" class="btn btn-info customfont">Créer</button>
