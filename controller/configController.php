9f3ad1b636263434c784065c59c231a68d097e94
Thomas Escorne <thomas.escorne@1001pneus.local>
2016-07-25 11:39:57 +0200

    formulaire de donnees vm

--- controller/configController.php
+++ controller/configController.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 if ($_POST['password'] != $_POST['conf-password']) {
   $_SESSION['ok-pass'] = 1;
   header('Location: http://interface-prox.www.1001pneus.fr/view/config_vm.php');
