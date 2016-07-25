0296a483a5e3f6e0843fed816d454f35b0e26064
Thomas Escorne <thomas.escorne@1001pneus.local>
2016-07-25 17:08:42 +0200

    test

--- controller/configController.php
+++ controller/configController.php
@@ -1,12 +1,14 @@
 <?php
 session_start();
 require_once(__DIR__ . "/../pve2_api.class.php");
+$template = "ct_template-1.4.tar.gz";
 $hostname = "10.100.1.19";
-$user = "VM_DEPLO";
+$user = "VM_DEPLOY";
 $realm = "pve";
 $password = "VKqdVZNHKyQD";
 
 $_SESSION['ok-pass'] = 0;
+$_SESSION['ok-log'] = 0;
 if (empty($_POST['password']) || empty($_POST['conf-password']) || empty($_POST['ID']) || empty($_POST['nom_vm']) || empty($_POST['disk_size']) || empty($_POST['CPU']) || empty($_POST['RAM']) || empty($_POST['swap'])) {
   $_SESSION['ok-empty'] = 1;
   header('Location: http://interface-prox.www.1001pneus.fr/view/config_vm.php');
@@ -23,7 +25,19 @@ $pve2 = new PVE2_API($hostname, $user, $realm, $password);
 if ($pve2->login()) {
     $_SESSION['ok-log'] = 0;
     $nodes = $pve2->get_node_list();
-
+    $first_node = $nodes[0];
+    unset($nodes);
+    $test = ($pve2->get_next_vmid());
+    echo $test;
+    /*$new_container_settings = array();
+    $new_container_settings['ostemplate'] = "local:vztmpl/" . $template;
+    $new_container_settings['vmid'] = $_POST['ID'];
+    $new_container_settings['cpus'] = $_POST['CPU'];
+    $new_container_settings['description'] = "VM 1001pneus";
+    $new_container_settings['disk'] = $_POST['disk_size'];
+    $new_container_settings['hostname'] = $_POST['nom_vm'];
+    $new_container_settings['memory'] = $_POST['RAM'];
+    $new_container_settings['nameserver'] = "4.2.2.1";*/
 } else {
     $_SESSION['ok-log'] = 1;
     header('Location: http://interface-prox.www.1001pneus.fr/view/config_vm.php');
