7512aaa6a6a58a5b68cf923d689b1eed75d99d6a
Thomas Escorne <thomas.escorne@1001pneus.local>
2016-07-25 14:32:24 +0200

    test

--- controller/configController.php
+++ controller/configController.php
@@ -1,20 +1,18 @@
 <?php
 session_start();
 
+$_SESSION['ok-pass'] = 0;
 if (empty($_POST['password']) || empty($_POST['conf-password']) || empty($_POST['ID']) || empty($_POST['nom_vm']) || empty($_POST['disk_size']) || empty($_POST['CPU']) || empty($_POST['RAM']) || empty($_POST['swap'])) {
   $_SESSION['ok-empty'] = 1;
   header('Location: http://interface-prox.www.1001pneus.fr/view/config_vm.php');
   die();
 }
-
+$_SESSION['ok-empty'] = 0;
 if ($_POST['password'] != $_POST['conf-password'] || strlen($_POST['password']) < 5) {
   $_SESSION['ok-pass'] = 1;
   header('Location: http://interface-prox.www.1001pneus.fr/view/config_vm.php');
   die();
 }
-
-
 $_SESSION['ok-pass'] = 0;
-$_SESSION['ok-empty'] = 0;
 header('Location: http://interface-prox.www.1001pneus.fr/view/endconf.php');
 ?>
