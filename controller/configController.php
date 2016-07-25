ef55cd035d69874b2193bfc16bb65ec165f8c5a5
Thomas Escorne <thomas.escorne@1001pneus.local>
2016-07-25 14:27:28 +0200

    test

--- controller/configController.php
+++ controller/configController.php
@@ -1,14 +1,14 @@
 <?php
 session_start();
 
-if ($_POST['password'] != $_POST['conf-password'] || strlen($_POST['password']) < 5) {
-  $_SESSION['ok-pass'] = 1;
+if (empty($_POST['password']) || empty($_POST['conf-password']) || empty($_POST['ID']) || empty($_POST['nom_vm']) || empty($_POST['disk_size']) || empty($_POST['CPU']) || empty($_POST['RAM']) || empty($_POST['swap'])) {
+  $_SESSION['ok-empty'] = 1;
   header('Location: http://interface-prox.www.1001pneus.fr/view/config_vm.php');
   die();
 }
 
-if (empty($_POST['password']) || empty($_POST['conf-password']) || empty($_POST['ID']) || empty($_POST['nom_vm']) || empty($_POST['disk_size']) || empty($_POST['CPU']) || empty($_POST['RAM']) || empty($_POST['swap'])) {
-  $_SESSION['ok-empty'] = 1;
+if ($_POST['password'] != $_POST['conf-password'] || strlen($_POST['password']) < 5) {
+  $_SESSION['ok-pass'] = 1;
   header('Location: http://interface-prox.www.1001pneus.fr/view/config_vm.php');
   die();
 }
