703cc68d59ca5a8f9dbef854b7001369d83792ca
Thomas Escorne <thomas.escorne@1001pneus.local>
2016-07-26 12:23:25 +0200

    test

--- pve2_api.class.php
+++ pve2_api.class.php
@@ -318,10 +318,8 @@ class PVE2_API {
 		}
 	}
 
-	public function get_vm_status ($node, $id)
-	 {
-	  $status = $this->get("/nodes/".$node."/lxc/".$id."/status/stop");
-		var_dump($status);
+	public function get_vm_status ($node, $id) {
+	  $status = $this->get("/nodes/".$node."/vz/".$id."/status/current");
 	  if ($status == null) {
 	    return false;
 	  } else {
