b3b147d42b66c11ebf47f1fb2eb1470b23783a4f
Thomas Escorne <thomas.escorne@1001pneus.local>
2016-07-26 10:55:30 +0200

    test

--- controller/configController.php
+++ controller/configController.php
@@ -40,13 +40,15 @@ if ($pve2->login()) {
     $new_container_settings['swap'] = $_POST['swap'];
     $new_container_settings['password'] = $_POST['password'];
     $pve2->post("/nodes/".$first_node."/openvz", $new_container_settings);
-    /*$i = 0;
+    $i = 0;
     while ($i < 15) {
+      ob_end_flush();
       $current_status = ($pve2->get_vm_status($first_node, $_POST['ID']));
       var_dump($current_status);
-    // sleep (60);
+      ob_start();
+      sleep (60);
       $i = $i + 1;
-    }*/
+    }
 
 } else {
     $_SESSION['ok-log'] = 1;
