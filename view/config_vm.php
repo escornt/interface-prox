ddc62cbc863d42d0c73b4d3748ed1c7de979ae49
Thomas Escorne <thomas.escorne@1001pneus.local>
2016-07-27 15:08:04 +0200

    test

--- view/config_vm.php
+++ view/config_vm.php
@@ -25,7 +25,7 @@
 <!-- ID VM -->
             <div class="form-group has-feedback">
               <div class="blu customfont" id="i1">ID VM</div>
-              <div id="txt1" style="visibility: hidden;">test</div>
+              <!-- <div id="txt1" style="visibility: hidden;">test</div> -->
               <input type="number" name="ID" class="form-control" id="ID VM" value="<?php echo $_SESSION['lastID'] ?>">
               <span class="glyphicon glyphicon-th-large form-control-feedback blu"></span>
             </div>
