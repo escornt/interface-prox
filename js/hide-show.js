6ed966f4f98e49ed277aab51f65acf97e966c91f
Thomas Escorne <thomas.escorne@1001pneus.local>
2016-07-27 16:11:12 +0200

    test

--- js/hide-show.js
+++ js/hide-show.js
@@ -1,16 +1,80 @@
 $('#i1').click(function(){
-
     if ($('#txt1').is(":visible")){
-        $('#txt1').hide();
-    }
-    else
-    {
-        $('#txt1').show();
-    }
-
+        $('#txt1').hide();}
+    else{$('#txt1').show();}
 });
-
-
 $('#txt1').ready(function(){
   $('#txt1').hide();
+});
+
+$('#i2').click(function(){
+    if ($('#txt2').is(":visible")){
+        $('#txt2').hide();}
+    else{$('#txt2').show();}
+});
+$('#txt2').ready(function(){
+  $('#txt2').hide();
+});
+
+$('#i3').click(function(){
+    if ($('#txt3').is(":visible")){
+        $('#txt3').hide();}
+    else{$('#txt3').show();}
+});
+$('#txt3').ready(function(){
+  $('#txt3').hide();
+});
+
+$('#i4').click(function(){
+    if ($('#txt4').is(":visible")){
+        $('#txt4').hide();}
+    else{$('#txt4').show();}
+});
+$('#txt4').ready(function(){
+  $('#txt4').hide();
+});
+
+$('#i5').click(function(){
+    if ($('#txt5').is(":visible")){
+        $('#txt5').hide();}
+    else{$('#txt5').show();}
+});
+$('#txt5').ready(function(){
+  $('#txt5').hide();
+});
+
+$('#i6').click(function(){
+    if ($('#txt6').is(":visible")){
+        $('#txt6').hide();}
+    else{$('#txt6').show();}
+});
+$('#txt6').ready(function(){
+  $('#txt6').hide();
+});
+
+$('#i7').click(function(){
+    if ($('#txt7').is(":visible")){
+        $('#txt7').hide();}
+    else{$('#txt7').show();}
+});
+$('#txt7').ready(function(){
+  $('#txt7').hide();
+});
+
+$('#i8').click(function(){
+    if ($('#txt8').is(":visible")){
+        $('#txt8').hide();}
+    else{$('#txt8').show();}
+});
+$('#txt8').ready(function(){
+  $('#txt8').hide();
+});
+
+$('#i9').click(function(){
+    if ($('#txt9').is(":visible")){
+        $('#txt9').hide();}
+    else{$('#txt9').show();}
+});
+$('#txt9').ready(function(){
+  $('#txt9').hide();
 });
--- view/config_vm.php
+++ view/config_vm.php
@@ -20,67 +20,70 @@
         </div>
     </div><!-- /.row -->
     <div class="row">
-      <div class="col-xs-5 col-md-offset-4.5">
+      <div class="col-xs-4 col-md-offset-4">
           <form class="form-vertical" role="form" method="POST" action="/controller/configController.php">
 <!-- ID VM -->
             <div class="form-group has-feedback">
               <label for="ID VM" class="blu customfont" id="i1">ID VM <i class="fa fa-caret-right"></i></label>
-              <div id="txt1"> <small>&nbsp Par défaut la dernière VM disponnible est sélectionnée. Choisir une VM déjà éxistante vous redirigera vers ce formulaire.</small></div>
+              <div id="txt1"> <small>&nbsp Par défaut la dernière VM disponnible est sélectionnée.</small></div>
               <input type="number" name="ID" class="form-control" id="ID VM" value="<?php echo $_SESSION['lastID'] ?>">
               <span class="glyphicon glyphicon-th-large form-control-feedback blu"></span>
             </div>
 <!-- Nom VM -->
             <div class="form-group has-feedback">
-              <label for="nom_vm" class="blu customfont">Nom VM</label>
+              <label for="nom_vm" class="blu customfont" id="i2">Nom VM<i class="fa fa-caret-right"></i></label>
+              <div id="txt2"> <small>&nbsp Par défaut le nom sera VM{VM_ID}.</small></div>
               <input type="text" name="nom_vm" class="form-control" id="nom_vm" value="<?php echo "VM".$_SESSION['lastID'] ?>">
               <span class="glyphicon glyphicon-tag form-control-feedback blu"></span>
             </div>
 <!-- Description -->
             <div class="form-group has-feedback">
-              <label for="description" class="blu customfont">Description</label>
+              <label for="description" class="blu customfont" id="i3">Description<i class="fa fa-caret-right"></i></label>
+              <div id="txt3"> <small>&nbsp La desciption permet aux autres utilisateurs de savoir à qui est la VM et à quoi elle sert.</small></div>
               <input type="text" name="description" class="form-control" id="description" value="VM de dev 1001pneus">
               <span class="glyphicon glyphicon-tags form-control-feedback blu"></span>
             </div>
 <!-- Mot de passe -->
             <div class="form-group has-feedback">
-              <label for="password" class="blu customfont">Mot de passe</label>
+              <label for="password" class="blu customfont" id="i4">Mot de passe<i class="fa fa-caret-right"></i></label>
+              <div id="txt4"> <small>&nbsp Mot de passe de la VM, Minimum 5 caractères.</small></div>
               <input type="password" name="password" class="form-control" id="password">
               <span class="glyphicon glyphicon-lock form-control-feedback blu"></span>
-              <div class="m3"><em><small>Minimum 5 caractères</small></em></div>
             </div>
 <!-- Confimation mot de passe -->
             <div class="form-group has-feedback">
-              <label for="password" class="blu customfont">Confirmer mot de passe</label>
+              <label for="password" class="blu customfont" id="i5">Confirmer mot de passe<i class="fa fa-caret-right"></i></label>
+              <div id="txt5"> <small>&nbsp Entrez le mot de passe une nouvelle fois pour le confirmer.</small></div>
               <input type="password" name="conf-password" class="form-control" id="conf-password">
               <span class="glyphicon glyphicon-lock form-control-feedback blu"></span>
             </div>
 <!-- Espace Disque -->
             <div class="form-group has-feedback">
-              <label for="Espace disque" class="blu customfont">Espace Disque (GB)</label>
+              <label for="Espace disque" class="blu customfont" id="i6">Espace Disque (GB)<i class="fa fa-caret-right"></i></label>
+              <div id="txt6"> <small>&nbsp Minimum recommandé -> 15 GB.</small></div>
               <input type="number" min="1" max="1000" name="disk_size" class="form-control" id="disk_size" value="15">
               <span class="glyphicon glyphicon-hdd form-control-feedback blu"></span>
-              <div class="m3"><em><small>Recommandation mini --> 15 GB</small></em></div>
             </div>
 <!-- CPUs -->
             <div class="form-group has-feedback">
-              <label for="CPU" class="blu customfont">CPUs</label>
+              <label for="CPU" class="blu customfont" id="i7">CPUs<i class="fa fa-caret-right"></i></label>
+              <div id="txt7"> <small>&nbsp Minimum recommandé -> 1.</small></div>
               <input type="number" min="1" name="CPU" class="form-control" id="CPU" value="1">
               <span class="glyphicon glyphicon-hdd form-control-feedback blu"></span>
-              <div class="m3"><em><small>Recommandation mini --> 1</small></em></div>
             </div>
 <!-- RAM -->
             <div class="form-group has-feedback">
-              <label for="RAM" class="blu customfont">RAM (MB)</label>
+              <label for="RAM" class="blu customfont" id="i8">RAM (MB)<i class="fa fa-caret-right"></i></label>
+              <div id="txt8"> <small>&nbsp Minimum recommandé -> 4000 MB.</small></div>
               <input type="number" min="512" max="16000" name="RAM" class="form-control" id="RAM" value="4000">
               <span class="glyphicon glyphicon-hdd form-control-feedback blu"></span>
-              <div class="m3"><em><small>Recommandation mini --> 4000 MB </small></em></div>
             </div>
 <!-- Swap -->
             <div class="form-group has-feedback">
-              <label for="swap" class="blu customfont">Swap (MB)</label>
+              <label for="swap" class="blu customfont" id="i9">Swap (MB)<i class="fa fa-caret-right"></i></label>
+              <div id="txt9"> <small>&nbsp Minimum recommandé -> 4000 MB.</small></div>
               <input type="number" min="512" max="16000" name="swap" class="form-control" id="swap" value="4000">
               <span class="glyphicon glyphicon-hdd form-control-feedback blu"></span>
-              <div class="m3"><em><small>Recommandation mini --> 4000 MB </small></em></div>
             </div>
 
   <!--          <div class="form-group has-feedback">
