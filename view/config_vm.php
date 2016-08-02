7a4eb80ef3c6f118208ac3c94cd8f5ea56c25785
Thomas Escorne <thomas.escorne@1001pneus.local>
2016-08-02 10:52:44 +0200

    test

--- view/config_vm.php
+++ view/config_vm.php
@@ -87,8 +87,8 @@
             </div>
 
             <div class="form-group has-feedback">
-              <input type="radio" name="template" value="default" class="blu customfont"> Template par défaut.
-              <input type="radio" name="template" value="mobile" class="blu customfont"> Template pour dev mobile.
+              <input type="radio" name="template" value="default"> <div class="blu customfont">Template par défaut. </div>
+              <input type="radio" name="template" value="mobile"> <div class="blu customfont">Template pour dev mobile. </div>
             </div>
 
   <!--          <div class="form-group has-feedback">
