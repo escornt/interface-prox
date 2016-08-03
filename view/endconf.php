de825d3b13f49e6c7cc59ace957f08f87ee7db69
Thomas Escorne <thomas.escorne@1001pneus.local>
2016-08-03 14:15:01 +0200

    test

--- view/endconf.php
+++ view/endconf.php
@@ -15,8 +15,7 @@
   <body>
     <div class="row">
         <div class="col-xs-5 col-md-offset-3">
-          <div class="m1 customfont blu fs2">La VM <?php echo $_SESSION['ID']; ?> à été créée avec succès</div>
-
+          <div class="m1 customfont blu fs2">La VM <?php echo $_SESSION['ID']; ?> a été créée avec succès</div>
         </div>
     </div><!-- /.row -->
   </body>
