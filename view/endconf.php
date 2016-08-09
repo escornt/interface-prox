c3f07cc10e93ca8675dd408433c7eae30c18f332
Thomas Escorne <thomas.escorne@1001pneus.local>
2016-08-09 10:53:11 +0200

    lello

--- view/endconf.php
+++ view/endconf.php
@@ -22,7 +22,7 @@
             Pour plus d'information et une configuration avancée
             <a href="http://wiki.1001pneus.fr/doku.php?id=it:it:interface_vm&#configuration_avancee">Cliquez ici.</a>
             <br/>
-            <a href="interface-prox.www.1001pneus.fr">Retour au portail de login</a>
+            <a href="/">Retour au portail de login</a>
           </p>
         </div>
     </div><!-- /.row -->
